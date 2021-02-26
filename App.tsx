ef9dbf8271035de7f32ec1c4cff064059a2cc526
Jelte Homminga <jelte.homminga@gmail.com>
2021-02-26 10:20:23 +0100

    moti-starter

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { AnimatePresence } from 'framer-motion'
-import { StyleSheet, Pressable } from 'react-native'
-import { View } from 'moti'
+import { AnimatePresence } from "framer-motion";
+import { StyleSheet, Pressable } from "react-native";
+import { View } from "moti";
 
 function Shape({ bg }) {
   return (
@@ -20,36 +20,37 @@ function Shape({ bg }) {
       }}
       style={[styles.shape, { backgroundColor: bg }]}
     />
-  )
+  );
 }
 
 export default function App() {
-  const [visible, toggle] = React.useReducer((s) => !s, true)
-
+  const [visible, toggle] = React.useReducer((s) => !s, true);
   return (
     <Pressable onPress={toggle} style={styles.container}>
+      {" "}
       <AnimatePresence exitBeforeEnter>
-        {visible && <Shape bg="hotpink" key="hotpink" />}
-        {!visible && <Shape bg="cyan" key="cyan" />}
-      </AnimatePresence>
+        {" "}
+        {visible && <Shape bg="hotpink" key="hotpink" />}{" "}
+        {!visible && <Shape bg="cyan" key="cyan" />}{" "}
+      </AnimatePresence>{" "}
     </Pressable>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   shape: {
-    justifyContent: 'center',
+    justifyContent: "center",
     height: 250,
     width: 250,
     borderRadius: 25,
     marginRight: 10,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    backgroundColor: '#9c1aff',
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    backgroundColor: "#9c1aff",
   },
-})
+});
